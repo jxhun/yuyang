cde84ac4e00cc2fd5f4c3dfd278f92c7376d001d
yanmin <949759703@qq.com>
2019-03-18 17:15:01 +0800

    第二次上传

---
+++ src/main/java/cn/com/yuyang/pojo/Kaoqin.java
@@ -0,0 +1,77 @@
+package cn.com.yuyang.pojo;
+
+
+public class Kaoqin {
+
+  private long id;
+  private long dangAnId;
+  private java.sql.Timestamp shangWuShangBan;
+  private java.sql.Timestamp xiaWuXiaBan;
+  private long zhuangTai;
+  private String qingJiaZhuangTai;
+  private java.sql.Date dangTianRiQi;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getDangAnId() {
+    return dangAnId;
+  }
+
+  public void setDangAnId(long dangAnId) {
+    this.dangAnId = dangAnId;
+  }
+
+
+  public java.sql.Timestamp getShangWuShangBan() {
+    return shangWuShangBan;
+  }
+
+  public void setShangWuShangBan(java.sql.Timestamp shangWuShangBan) {
+    this.shangWuShangBan = shangWuShangBan;
+  }
+
+
+  public java.sql.Timestamp getXiaWuXiaBan() {
+    return xiaWuXiaBan;
+  }
+
+  public void setXiaWuXiaBan(java.sql.Timestamp xiaWuXiaBan) {
+    this.xiaWuXiaBan = xiaWuXiaBan;
+  }
+
+
+  public long getZhuangTai() {
+    return zhuangTai;
+  }
+
+  public void setZhuangTai(long zhuangTai) {
+    this.zhuangTai = zhuangTai;
+  }
+
+
+  public String getQingJiaZhuangTai() {
+    return qingJiaZhuangTai;
+  }
+
+  public void setQingJiaZhuangTai(String qingJiaZhuangTai) {
+    this.qingJiaZhuangTai = qingJiaZhuangTai;
+  }
+
+
+  public java.sql.Date getDangTianRiQi() {
+    return dangTianRiQi;
+  }
+
+  public void setDangTianRiQi(java.sql.Date dangTianRiQi) {
+    this.dangTianRiQi = dangTianRiQi;
+  }
+
+}
---
+++ src/main/java/cn/com/yuyang/pojo/Zhiwubiao.java
@@ -0,0 +1,117 @@
+package cn.com.yuyang.pojo;
+
+
+public class Zhiwubiao {
+
+  private long id;
+  private String zhiWuMingCheng;
+  private String zhiWuJianJie;
+  private long chaKanKaoQin;
+  private long qinJiaShenPi;
+  private long buMenCaoZuo;
+  private long chaKanYuanGong;
+  private long caoZuoYuanGong;
+  private long _FaBuXiuGaiGongGao;
+  private long daiBanShiXiang;
+  private java.sql.Timestamp chuangJianShiJian;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getZhiWuMingCheng() {
+    return zhiWuMingCheng;
+  }
+
+  public void setZhiWuMingCheng(String zhiWuMingCheng) {
+    this.zhiWuMingCheng = zhiWuMingCheng;
+  }
+
+
+  public String getZhiWuJianJie() {
+    return zhiWuJianJie;
+  }
+
+  public void setZhiWuJianJie(String zhiWuJianJie) {
+    this.zhiWuJianJie = zhiWuJianJie;
+  }
+
+
+  public long getChaKanKaoQin() {
+    return chaKanKaoQin;
+  }
+
+  public void setChaKanKaoQin(long chaKanKaoQin) {
+    this.chaKanKaoQin = chaKanKaoQin;
+  }
+
+
+  public long getQinJiaShenPi() {
+    return qinJiaShenPi;
+  }
+
+  public void setQinJiaShenPi(long qinJiaShenPi) {
+    this.qinJiaShenPi = qinJiaShenPi;
+  }
+
+
+  public long getBuMenCaoZuo() {
+    return buMenCaoZuo;
+  }
+
+  public void setBuMenCaoZuo(long buMenCaoZuo) {
+    this.buMenCaoZuo = buMenCaoZuo;
+  }
+
+
+  public long getChaKanYuanGong() {
+    return chaKanYuanGong;
+  }
+
+  public void setChaKanYuanGong(long chaKanYuanGong) {
+    this.chaKanYuanGong = chaKanYuanGong;
+  }
+
+
+  public long getCaoZuoYuanGong() {
+    return caoZuoYuanGong;
+  }
+
+  public void setCaoZuoYuanGong(long caoZuoYuanGong) {
+    this.caoZuoYuanGong = caoZuoYuanGong;
+  }
+
+
+  public long get_FaBuXiuGaiGongGao() {
+    return _FaBuXiuGaiGongGao;
+  }
+
+  public void set_FaBuXiuGaiGongGao(long _FaBuXiuGaiGongGao) {
+    this._FaBuXiuGaiGongGao = _FaBuXiuGaiGongGao;
+  }
+
+
+  public long getDaiBanShiXiang() {
+    return daiBanShiXiang;
+  }
+
+  public void setDaiBanShiXiang(long daiBanShiXiang) {
+    this.daiBanShiXiang = daiBanShiXiang;
+  }
+
+
+  public java.sql.Timestamp getChuangJianShiJian() {
+    return chuangJianShiJian;
+  }
+
+  public void setChuangJianShiJian(java.sql.Timestamp chuangJianShiJian) {
+    this.chuangJianShiJian = chuangJianShiJian;
+  }
+
+}
