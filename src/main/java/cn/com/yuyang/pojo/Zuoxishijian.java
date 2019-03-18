5e1facc6d064c3d05c153ac7cbc72a1301025413
yanmin <949759703@qq.com>
2019-03-18 17:29:03 +0800

    第三次上传

---
+++ src/main/java/cn/com/yuyang/pojo/Zuoxishijian.java
@@ -0,0 +1,37 @@
+package cn.com.yuyang.pojo;
+
+
+public class Zuoxishijian {
+
+  private long id;
+  private java.sql.Timestamp shangWuShangBan;
+  private java.sql.Timestamp xiaWuXiaBan;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public java.sql.Timestamp getShangWuShangBan() {
+    return shangWuShangBan;
+  }
+
+  public void setShangWuShangBan(java.sql.Timestamp shangWuShangBan) {
+    this.shangWuShangBan = shangWuShangBan;
+  }
+
+
+  public java.sql.Timestamp getXiaWuXiaBan() {
+    return xiaWuXiaBan;
+  }
+
+  public void setXiaWuXiaBan(java.sql.Timestamp xiaWuXiaBan) {
+    this.xiaWuXiaBan = xiaWuXiaBan;
+  }
+
+}
