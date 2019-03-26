e245804bd318dc6b615a797894437a74d7977c09
yanmin <949759703@qq.com>
2019-03-26 14:42:52 +0800

    14:3 3/26 再次整合

---
+++ src/main/java/cn/com/yuyang/pojo/Mingancaozuo.java
@@ -0,0 +1,57 @@
+package cn.com.yuyang.pojo;
+
+
+public class Mingancaozuo {
+
+  private long id;
+  private long dangAnId;
+  private String fangFaMingCheng;
+  private String returnNeiRong;
+  private java.sql.Timestamp shiJian;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public long getDangAnId() {
+    return dangAnId;
+  }
+
+  public void setDangAnId(long dangAnId) {
+    this.dangAnId = dangAnId;
+  }
+
+
+  public String getFangFaMingCheng() {
+    return fangFaMingCheng;
+  }
+
+  public void setFangFaMingCheng(String fangFaMingCheng) {
+    this.fangFaMingCheng = fangFaMingCheng;
+  }
+
+
+  public String getReturnNeiRong() {
+    return returnNeiRong;
+  }
+
+  public void setReturnNeiRong(String returnNeiRong) {
+    this.returnNeiRong = returnNeiRong;
+  }
+
+
+  public java.sql.Timestamp getShiJian() {
+    return shiJian;
+  }
+
+  public void setShiJian(java.sql.Timestamp shiJian) {
+    this.shiJian = shiJian;
+  }
+
+}
